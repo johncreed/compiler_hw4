1ca10b7f6731a75c3492b197735a4bea2c9fbd81
johncreed <hsiajuiyang5174@gmail.com>
2020-06-07 12:47:01 +0800

    Update test flow

--- run_and_main/output.s
+++
@@ -1,8 +0,0 @@
-.text
-_start_MAIN:
-	stp x29, x30, [sp, -16]!
-	add x29, sp, 0
-	mov x0, 10
-	bl _write_int
-	ldp x29, x30, [sp], 16
-	ret
--- run_and_main/run.sh
+++ run_and_main/run.sh
@@ -1,3 +1,8 @@
+#! /bin/bash
 
-aarch64-linux-gnu-gcc-4.8 --static -O0 $1
+# Generate output.s
+./parser $1 > output.s
+
+# Compiler to bin and run on aarch64 emulator
+aarch64-linux-gnu-gcc-4.8 --static -O0 main.S
 qemu-aarch64 a.out
